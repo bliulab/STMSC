8f7f4d321b4ce09b817e76ce7a302fdacf219b1a
bliulab <bliu@bliulab.net>
2025-05-15 16:35:35 +0800

    setup.py

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     ],
     classifiers=[
         "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires="==3.10",
