86b83090de6b9c9755309c132d78d9c0ad8c9dad
bliulab <bliu@bliulab.net>
2025-07-02 14:06:10 +0800

    Update networks.py

--- STMSC/networks.py
+++ STMSC/networks.py
@@ -5,15 +5,7 @@ import torch.optim as optim
 import numpy as np
 from torch.nn.parameter import Parameter
 
-import random
-seed = 666
-random.seed(seed)
-np.random.seed(seed)
-torch.manual_seed(seed)
-torch.cuda.manual_seed(seed)
-torch.cuda.manual_seed_all(seed)
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False
+
 
 class DeconvNet1(nn.Module):
 
